a8a9d4459d1eed7e4da3ab449b05724a56b8cc01
Cyber <cyberchaud@gmail.com>
2020-06-07 09:34:13 -0400

    Video 1 done

---
+++ vid01/vid01.py
@@ -0,0 +1,40 @@
+# Python always evaluates down to a single value.
+# Integers are whole number
+# Any digits with a decimal is a floating point
+
+# Expresssions are made from Values and Operators
+
+print("Adding")
+print(2+2)
+print("Substraction")
+print(5-3)
+print("Multiplication")
+print(3*7)
+print("Division")
+print(21/7)
+
+print("PEMDAS")
+print(2+3*6)
+
+# (2+3)*6
+# Python does:
+#  (2+3) = 5
+# (5)*6
+# 30
+print((2+3)*6)
+
+# String
+# Strings are for text
+# Concatenation for strings
+
+print('Alice')
+print('Alice' + 'Bob')
+
+#String Replication
+print('Alice'*3)
+
+# Variables
+
+spam = 'Hello'
+print(spam)
+print(spam + ' World')
