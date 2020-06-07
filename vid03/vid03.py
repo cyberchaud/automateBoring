123f466c54ace8478bb1767fa7532bf301d37d11
Cyber <cyberchaud@gmail.com>
2020-06-07 10:09:49 -0400

    Video 3

---
+++ vid03/vid03.py
@@ -0,0 +1,50 @@
+# Datatypes
+
+# boolean - can either be True or False
+# integers and strings can be
+# Integers and strings will never be true
+# Integers and float values can be true
+
+
+myBool = True
+print('My bool is {}'.format(str(myBool)))
+
+# Comparision Operators
+# ==
+# !=
+# <
+# >
+# <=
+# >=
+
+# This will print True
+print('42 == 42')
+print(42 == 42)
+
+# This will print True
+print('42.0 == 42')
+print(42.0 == 42)
+
+# This will print False
+print('42 == ''''42''''')
+print(42 == '42')
+
+# This will print True
+print('True == True')
+print(True == True)
+
+# This will print False
+print('False == True')
+print(False == True)
+
+# This will print True
+print('False == False')
+print(False == False)
+
+# This will print False
+print('not True')
+print(not True)
+
+# This will print
+print('not False')
+print(not False)
