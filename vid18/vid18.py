2d6f27cfaddf6e391817e31fc9deb1ac200d4e66
Cyber <cyberchaud@gmail.com>
2020-06-14 10:48:29 -0400

    Video 18

---
+++ vid18/vid18.py
@@ -0,0 +1,33 @@
+# Escape characters in Python
+# \' - single quote
+# \" - double quote
+# \t - Tab
+# \n - Newline
+# \\ - Backslash
+
+# string = 'Alice\'s'
+# result of print = Alice
+
+print('Alice\'s')
+
+# rawstring in Python is started with a small r
+# rawstrings will not escape any characters
+# for rawstrings
+
+print(r'That cat is Carol\'s cat')
+
+# Multiline line strings are identified with triple quotes """
+print("""That cat 
+is
+Carol\'s 
+cat""")
+
+# strings are list-like variables
+# all the same things that lists can do can be done on strings
+
+spam = 'This is my text'
+print(spam[-3])
+print(spam[-3:])
+print(spam[:-3])
+print('z' in spam)
+print('x' in spam)
