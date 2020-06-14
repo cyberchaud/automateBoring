080233441a6951ed76a9cf39269b9bf9b61a3c48
Cyber <cyberchaud@gmail.com>
2020-06-14 12:11:08 -0400

    Video 21

---
+++ vid21/vid21.py
@@ -0,0 +1,27 @@
+#! /usr/bin/python3
+
+# Python code should start with a she-bang line
+# for Linux /usr/bin/python3
+
+# pause command for Windows
+# read -p "Message" for Bash
+
+# in a batch file - @pyw will run a python script in windowless mode
+
+print('Hello World')
+
+# PATH environment
+# the PATH is a list of directories that tells the shell which directories to search for executable files
+
+# in Linux
+# env shows all the current environmental variables
+# env | grep PATH shows only the PATH alone
+# echo $PATH shows the contents of just the PATH alone
+
+# PATH can be changed just for the current login session
+# PATH="/usr/sbin:$PATH"
+# or permanently
+# bit editing .bash_profile file
+# .bash_profile is a hidden in a linux user's home directory
+
+# probably best to use the ~/.bashrc
