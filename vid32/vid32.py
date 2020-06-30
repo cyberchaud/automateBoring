b6d581e4c9a5f0c03959530a7334b2df177b45b8
Cyber <cyberchaud@gmail.com>
2020-06-30 17:23:07 -0400

    Video 32

---
+++ vid32/vid32.py
@@ -0,0 +1,10 @@
+# shutil performs file operations like mv and cp
+
+# important methods are:
+# copy - which copies a file
+# move - which moves a file
+# move - can also be used to rename a file
+# copytree - which copies an entire directory's contents
+
+import shutil
+
